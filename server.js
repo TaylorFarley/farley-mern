5899feab6bc3144aaf965fc59d0be702f8bc1208
Taylor Farley <twfarley88@gmail.com>
2020-08-23 16:23:58 -0400

    adding in google auth callback

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const app = express()
+const keys = require('./config/keys')
 
+passport.use(new GoogleStrategy
+    (
+    {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    },(accessToken)=>{
+        console.log(accessToken)
+    }
+    )
+    )
 
-app.get('/',(req,res)=>{
-    res.send('hello world')
-})
+    app.get('/auth/google', passport.authenticate('google',{
+        scope: ['profile','email']
+    }))
 
-
+    app.get('/auth/google/callback',(req,res)=>{
+        res.send('hi')
+        
+    })
 const PORT = process.env.PORT || 3000
 app.listen(PORT,()=>{
     console.log('runnin and runnin and runnin')
