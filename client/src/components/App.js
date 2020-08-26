72d36a9fedd691fe8fef325711789e8c2796b43d
Taylor Farley <twfarley88@gmail.com>
2020-08-26 11:06:44 -0400

    browser router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,3 +1,29 @@
 //underneath the index component
 //rentdering layer, the react layer
 //what components to show on the screen
+import React from 'react'
+import { BrowserRouter, Route} from 'react-router-dom'
+import Header from './Header'
+
+const Dashboard = ()=><h2>Dashbaord</h2>
+const SurveyNew = ()=><h2>SurveyNew</h2>
+const Landing = ()=><h2>Landing</h2>
+
+const App = ()=>{
+    return(
+        <div className="container">
+            <BrowserRouter>
+            <div>
+                <Header />
+                <Route exact path="/" component={Landing} />
+                <Route exact path="/surveys"  component={Dashboard} />
+                <Route exact path="/surveys/new" component={SurveyNew} />
+               
+            </div>
+            </BrowserRouter>
+
+        </div>
+    )
+}
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,19 @@
-//redux
+import 'materialize-css/dist/css/materialize.min.css'
 import React from 'react'
 import ReactDom from 'react-dom'
+import {
+    Provider
+} from 'react-redux'
+import {
+    createStore,
+    applyMiddleware
+} from 'redux'
+import App from './components/App'
+import reducers from './reducers'
+const store = createStore(reducers, {}, applyMiddleware())
 
-ReactDom.render()
 
+ReactDom.render(
+    <Provider store={store}><App/></Provider>,
+    document.querySelector('#root')
+    )
